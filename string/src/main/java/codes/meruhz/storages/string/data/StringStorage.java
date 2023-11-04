bd03cfa1d0b98c89e7973c2d0dd729fd466e5c49
Meruhz <meruhz.contato@gmail.com>
2023-11-03 23:48:32 -0300

    Back to Java 8
    Signed-off-by: Meruhz <meruhz.contato@gmail.com>

--- string/src/main/java/codes/meruhz/storages/string/data/StringStorage.java
+++ string/src/main/java/codes/meruhz/storages/string/data/StringStorage.java
@@ -4,11 +4,7 @@ import codes.meruhz.storages.core.data.impl.AbstractStorage;
 import codes.meruhz.storages.string.StringStorages;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.Locale;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class StringStorage extends AbstractStorage<String, Locale> {
 
@@ -16,21 +12,7 @@ public class StringStorage extends AbstractStorage<String, Locale> {
         super(name, defaultLocale);
     }
 
-    @Override
-    protected @NotNull CompletableFuture<Void> stop() {
-        @NotNull CompletableFuture<Void> completableFuture = new CompletableFuture<>();
-
-        CompletableFuture.runAsync(() -> {
-
-            try {
-                super.getJsonContent().setConfiguration(StringStorages.getInstance().getStorageSerializer().serialize(this), true);
-                completableFuture.complete(null);
-
-            } catch (Throwable throwable) {
-                completableFuture.completeExceptionally(throwable);
-            }
-        });
-
-        return completableFuture.orTimeout(Duration.of(20, ChronoUnit.SECONDS).toMillis(), TimeUnit.MILLISECONDS);
+    public void unload() {
+        super.getJsonContent().setConfiguration(StringStorages.getInstance().getStorageSerializer().serialize(this), true);
     }
 }
